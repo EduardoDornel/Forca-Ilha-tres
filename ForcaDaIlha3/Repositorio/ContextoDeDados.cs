f564c8e6fa57125f9d90b1b7eb5cc17b1a6d463c
daniel.figueiredo <daniel.carvalho.figueiredo@gmail.com>
2016-11-11 18:13:31 -0200

    feat(Forca/Repositorio): Adicionar contexto de dados e entityframework.

---
+++ ForcaDaIlha3/Repositorio/ContextoDeDados.cs
@@ -0,0 +1,25 @@
+﻿using ForcaDaIlha3.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcaDaIlha3.Repositorio
+{
+    class ContextoDeDados : DbContext
+    {
+        public ContextoDeDados() :base("Forca") {}
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Palavra> Palavra { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
