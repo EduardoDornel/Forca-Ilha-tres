e221b4ce1ce28fc65c40ceef4f9e9dec91f63446
daniel.figueiredo <daniel.carvalho.figueiredo@gmail.com>
2016-11-11 18:12:35 -0200

    feat(Repositorio/Migrations): Adicionar migration inicial do banco de dados.

---
+++ ForcaDaIlha3/Repositorio/Migrations/201611111944115_Initial.cs
@@ -0,0 +1,39 @@
+namespace ForcaDaIlha3.Repositorio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Palavra",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Texto = c.String(),
+                        Dica = c.String(),
+                        TamanhoDaPalavra = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Usuario",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Pontuacao = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Usuario");
+            DropTable("dbo.Palavra");
+        }
+    }
+}
