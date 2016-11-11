cdb20e4e27b4d2eddd78449bd17bd05d0ee730fa
daniel.figueiredo <daniel.carvalho.figueiredo@gmail.com>
2016-11-11 18:11:38 -0200

    feat(Dominio): Adicionar classes para Usuario e Palavra

---
+++ ForcaDaIlha3/Dominio/Palavra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcaDaIlha3.Dominio
+{
+    public class Palavra
+    {
+        public Palavra() {}
+
+        public int Id { get; set; }        
+        public string Texto { get; set; }
+        public string Dica { get; set; }
+        public int TamanhoDaPalavra { get; set; }
+    }
+}
---
+++ ForcaDaIlha3/Dominio/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcaDaIlha3.Dominio
+{
+    public class Usuario
+    {
+        public Usuario() {}
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Pontuacao { get; set; }
+    }
+}
