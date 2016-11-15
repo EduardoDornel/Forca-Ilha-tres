b8c737b33a6e6f4e37b42e0e0812cc50228f89da
Matheus Schmitz <augs.math@gmail.com>
2016-11-15 21:56:16 -0200

    feat(TelaAviso): exibir game-over e vitoria em uma tela com opções de reiniciar, ir para leaderboard e voltar para o menu.

--- ForcaDaIlha3/ForcaDaIlha3/Scripts/app/telaAviso.js
+++ ForcaDaIlha3/ForcaDaIlha3/Scripts/app/telaAviso.js
@@ -1,1 +1,63 @@
-﻿
+﻿class TelaAviso {
+
+    constructor(seletor) {
+        this.$elem = $(seletor);
+        this.dificuldade = window.localStorage.getItem('dificuldade');
+        this.aviso = seletor.substring(6, 15);
+        this.renderizarEstadoInicial();
+    }
+
+    registrarBindsEventos() {
+        this.$btnReiniciar = $('#btn-reiniciar');
+        this.$btnVoltarMenu = $('#btn-voltar-menu');
+        this.$btnLeaderboard = $('#btn-leaderboard');
+        this.$btnReiniciar.on('click', this.reiniciar.bind(this));
+        this.$btnVoltarMenu.on('click', this.renderizarMenu);
+        this.$btnLeaderboard.on('click', this.renderizarLeaderboard);
+    }
+    montarMensagem() {
+        let msg = '';
+        if (this.aviso === 'game-over') {
+            msg = this.aviso;
+        }
+        else if(this.dificuldade === 'normal')
+        {
+            msg = 'parabéns, você zerou o modo normal!';
+        }
+        else
+        {
+            msg = 'nossa, certeza que você estava de hack... parabéns, você zerou o modo mais extremely hard badass, vulgo BH!!!';
+        }
+        this.renderizarMensagem(msg.toUpperCase());
+    }
+    renderizarMensagem(msg) {
+        let self = this;
+        forca.render('.tela', 'tela-aviso', {
+            mensagem: msg
+        }).then(() => {
+            self.registrarBindsEventos(self)
+        });
+    }
+
+    renderizarMenu() {
+        window.location.reload();
+    }
+    renderizarLeaderboard() {
+        forca.renderizarTela('placar');
+    }
+    reiniciar() {
+        let self = this;
+        $.get('/api/jogo', { dificuldade: self.dificuldade })
+            .done(function (res) {
+                let idsPalavras = res.dados;
+                window.localStorage.setItem('ids-palavras', JSON.stringify(idsPalavras));
+                forca.renderizarTela(self.dificuldade);
+            });
+    }
+
+
+    renderizarEstadoInicial() {
+        this.$elem.show();
+        this.montarMensagem();
+    }
+}
--- ForcaDaIlha3/ForcaDaIlha3/Scripts/app/telaJogo.js
+++ ForcaDaIlha3/ForcaDaIlha3/Scripts/app/telaJogo.js
@@ -57,14 +57,23 @@
         let pontuacao = window.localStorage.getItem('pontuacao');
         let idUsuario = window.localStorage.getItem('id-usuario');
         $.get('/api/jogo', { pontos: pontuacao, idUsuario: idUsuario, dificuldade: self.dificuldade })
-            .done(function (res) {
-                self.reiniciar(self);
+            .done(function () {
+                self.renderizarAviso('game-over');
             })
     }
     ganhou(pontuacaoDaRodada) {
         let pontuacao = window.localStorage.getItem('pontuacao');
         window.localStorage.setItem('pontuacao', JSON.parse(pontuacao) + pontuacaoDaRodada);
-        forca.renderizarTela(this.dificuldade);
+        let qtdPalavrasRestantes = JSON.parse(window.localStorage.getItem('ids-palavras')).length;
+        if (qtdPalavrasRestantes === 0) {
+            this.renderizarAviso('vitoria');
+        } else {
+            forca.renderizarTela(this.dificuldade);
+        }
+    }
+    renderizarAviso(status) {
+        window.localStorage.setItem('dificuldade', this.dificuldade);
+        forca.renderizarTela(status);
     }
     pegarPalavra(self) {
         let ids = window.localStorage.getItem('ids-palavras');
@@ -103,7 +112,6 @@
         });
     }
     reiniciar(self) {
-        var dif = self.dificuldade;
         $.get('/api/jogo', { dificuldade: self.dificuldade })
             .done(function (res) {
                 let idsPalavras = res.dados;
